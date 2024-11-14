1e64fd2a63447b4b5ce0f849bc02d5679213a4c7
Aktan <motivationaktan@gmail.com>
2024-11-14 10:28:04 +0600

    react-lesson3

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,16 +7,16 @@ function Header() {
                 <div>
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy6BGhj2_dqApdaLKCa-8uD62wTGtxJp1tkQ&s" alt="" />
                 </div>
-                <a href="https://dodopizza.kg/bishkek#wxgwy">Завтраки</a>
-                <a href="https://dodopizza.kg/bishkek#zrmzo">Пицца</a>
-                <a href="https://dodopizza.kg/bishkek#xqhej">Комбо</a>
-                <a href="https://dodopizza.kg/bishkek#ancfo">Закуски</a>
-                <a href="https://dodopizza.kg/bishkek#pcexx">Десерты</a>
-                <a href="https://dodopizza.kg/bishkek#qlguj">Кофе</a>
-                <a href="https://dodopizza.kg/bishkek#gcydj">Напитки</a>
-                <a href="https://dodopizza.kg/bishkek#qqzxt">Соусы</a>
-                <a href="https://dodopizza.kg/bishkek#xnbrz">Другие товары</a>
-                <a href="https://dodopizza.kg/bishkek/bonusactions">Акции</a>
+                <a className='a-active' href="https://dodopizza.kg/bishkek#wxgwy">Завтраки</a>
+                <a className='a-active' href="https://dodopizza.kg/bishkek#zrmzo">Пицца</a>
+                <a className='a-active' href="https://dodopizza.kg/bishkek#xqhej">Комбо</a>
+                <a className='a-active' href="https://dodopizza.kg/bishkek#ancfo">Закуски</a>
+                <a className='a-active' href="https://dodopizza.kg/bishkek#pcexx">Десерты</a>
+                <a className='a-active' href="https://dodopizza.kg/bishkek#qlguj">Кофе</a>
+                <a className='a-active' href="https://dodopizza.kg/bishkek#gcydj">Напитки</a>
+                <a className='a-active' href="https://dodopizza.kg/bishkek#qqzxt">Соусы</a>
+                <a className='a-active' href="https://dodopizza.kg/bishkek#xnbrz">Другие товары</a>
+                <a className='a-active' href="https://dodopizza.kg/bishkek/bonusactions">Акции</a>
             </div>
             <div className="right">
                 <button className='btn'>Корзина</button>
